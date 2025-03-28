f1196473f07aae2b100061e7b1edc92b7785a3ba
AdoFada1 <120673728+AdoFada1@users.noreply.github.com>
2025-03-28 15:57:03 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 ﻿document.addEventListener("DOMContentLoaded", function() {
     console.log("YITDA Website Loaded");
 });
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Business registered successfully! (Backend coming soon)");
+});
+
